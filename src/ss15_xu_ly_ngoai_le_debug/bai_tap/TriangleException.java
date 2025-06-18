16eecfc7e589f3df1ad57e03e84a89f1faaf77fa
Nguyen Trung Hieu <hieugrth13@gmail.com>
2025-06-18 19:06:29 +0700

    update ss15

---
+++ src/ss15_xu_ly_ngoai_le_debug/bai_tap/TriangleException.java
@@ -0,0 +1,32 @@
+package ss15_xu_ly_ngoai_le_debug.bai_tap;
+
+import java.util.Scanner;
+
+public class TriangleException {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Nhập cạnh a: ");
+            int a = Integer.parseInt(sc.nextLine());
+            System.out.println("Nhập cạnh b: ");
+            int b = Integer.parseInt(sc.nextLine());
+            System.out.println("Nhập cạnh c: ");
+            int c = Integer.parseInt(sc.nextLine());
+            checkTriangle(a, b, c);
+        } catch (IllegalTriangleException e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Lỗi" + e.getMessage());
+        }
+    }
+
+    public static void checkTriangle(int a, int b, int c) throws IllegalTriangleException {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleException("Cạnh tam giác phải là số dương");
+        }
+
+        if (a+b<c || b+c<a || c+a<b) {
+            throw new IllegalTriangleException("Tổng hai cạnh bất kỳ phải lớn hơn cạnh còn lại");
+        }
+    }
+}
